8134048af89103d56988ae1b02abce2832192668
unknown <eduardolobo.esp@gmail.com>
2023-07-04 18:56:09 -0300

    Crinado converters para redefinir a exibição dos valores

---
+++ ExpenseControl/Converters/TransactionColorValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseControl.Models;
+using System.Globalization;
+
+namespace ExpenseControl.Converters
+{
+    internal class TransactionColorValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+
+            if (transaction == null) return Colors.Black;
+
+            return transaction.Type == TransactionType.Expenses ? Colors.Red : Colors.Green;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
---
+++ ExpenseControl/Converters/TransactionValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseControl.Models;
+using System.Globalization;
+
+namespace ExpenseControl.Converters
+{
+    internal class TransactionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+            if (transaction == null) return string.Empty;
+
+            return transaction.Type == TransactionType.Expenses ? $"- {transaction.Value.ToString("C")}" : transaction.Value.ToString("C");
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
